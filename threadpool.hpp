f3d4ef3a35d1c866236d5a524202cc25f4d93a17
z2w <15000941927@163.com>
2025-08-15 09:36:58 +0800

    新增交换队列

--- threadpool.hpp
+++ threadpool.hpp
@@ -49,6 +49,54 @@ private:
     std::condition_variable not_empty_;
 };
 
+template <typename T>
+class SwapBlockingQueue
+{
+public:
+    SwapBlockingQueue(bool nonblock = false) : nonblock_(nonblock) {}
+        /*入队操作*/
+    void push(const T& value) {
+        std::lock_guard<std::mutex> lock(prod_mutex_);
+        prod_queue_.push(value);
+        not_empty_.notify_one(); /*唤醒一个等待线程*/
+    } 
+
+    bool pop(T& value) {
+        std::unique_lock<std::mutex> lock(cons_mutex_); /*unique_lock可以手动调用unlock*/
+        if (cons_queue_.empty()) {
+            if (0 == swap_queue()) {
+                return false;
+            }
+        }
+        value = cons_queue_.front();
+        cons_queue_.pop();
+        return true;
+
+    }
+
+    /*解除阻塞接口，用于唤醒休眠线程*/
+    void cancel() {
+        nonblock_ = true;
+        not_empty_.notify_all(); /*唤醒所有等待线程*/
+    } 
+
+private:
+    int swap_queue() {
+        std::unique_lock<std::mutex> lock(prod_mutex_);
+        not_empty_.wait(lock, [this] (){
+            return !this->prod_queue_.empty() || this->nonblock_; /*等待生产队列非空或非阻塞*/
+        });
+        std::swap(prod_queue_, cons_queue_); /*交换生产和消费队列*/
+        return cons_queue_.size(); /*返回消费队列的大小*/
+    }
+    bool nonblock_;
+    std::queue<T> prod_queue_;
+    std::queue<T> cons_queue_;
+    std::mutex prod_mutex_;
+    std::mutex cons_mutex_;
+    std::condition_variable not_empty_;
+};
+
 class ThreadPool
 {
 public:
@@ -84,7 +132,7 @@ private:
             task(); /*执行任务*/
         }
     }
-    BlockingQueue<std::function<void()>> task_queue_; /*阻塞队列，任务队列为空，阻塞线程*/
+    SwapBlockingQueue<std::function<void()>> task_queue_; /*阻塞队列，任务队列为空，阻塞线程*/
     std::vector<std::thread> workers_;
 };
 
